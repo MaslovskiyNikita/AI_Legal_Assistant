a01a7acf0a897934bcfe405eb771edf09cafce6e
stanislav <s.markovtsev@gmail.com>
2026-03-16 16:13:49 +0300

    added docker multistage building

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Запуск миграций базы данных (Alembic)..."
-poetry run alembic revision --autogenerate -m "Initial migration: create users table"
-poetry run alembic upgrade head
+alembic revision --autogenerate -m "Initial migration: create users table"
+alembic upgrade head
 
 echo "Запуск сидера (заполнение тестовыми данными)..."
-poetry run python -c "
+python -c "
 import asyncio
 from app.core.database import AsyncSessionLocal
 from app.seed import seed_db
