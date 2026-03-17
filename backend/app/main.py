8c0dd2e9193069e74e08597481237016073d0fb8
stanislav <s.markovtsev@gmail.com>
2026-03-18 00:19:04 +0300

    add logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,27 @@
+import logging
+import sys
+from app.utils.logging import InterceptHandler
+from loguru import logger
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import app.models.user 
 import app.models.chat
 from app.api.v1 import auth, chats, documents
+
+logging.getLogger("uvicorn.access").handlers = []
+logging.getLogger("uvicorn.error").handlers = []
+logging.getLogger("sqlalchemy.engine").handlers = []
+
+logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
+
+logger.configure(handlers=[{
+    "sink": sys.stdout, 
+    "format": "<green>{time:HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{message}</cyan>",
+    "colorize": True 
+}])
+
+logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
 app = FastAPI(title="LegalAI Assistant API")
 
