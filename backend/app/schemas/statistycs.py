45f012c9df280c69b0cfbc6b88a502255396ab2d
MaslovskiyNikita <nikitamaslovskiy999@gmail.com>
2026-03-19 21:50:25 +0400

    update

---
+++ backend/app/schemas/statistycs.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Any, List, Optional
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class RiskStatisticResponse(BaseModel):
+    risk_type: str
+    count: int
+    
+class ActivityStatisticResponse(BaseModel):
+    date: str
+    message_count: int
