85bd80213caad6c883e040876663733392712364
MaslovskiyNikita <nikitamaslovskiy999@gmail.com>
2026-03-19 15:12:31 +0400

    payment

--- backend/app/core/prices.py
+++ backend/app/core/prices.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from typing import ClassVar
 
 @dataclass
 class PackagePrices:
-    "pack_100" = {"tokens": 100, "stars": 1}
-    "pack_500" = {"tokens": 500, "stars": 2}
-    "pack_1000" = {"tokens": 1000, "stars": 3}
+    pack_100: ClassVar[dict] = {"tokens": 100, "stars": 1}
+    pack_500: ClassVar[dict] = {"tokens": 500, "stars": 2}
+    pack_1000: ClassVar[dict] = {"tokens": 1000, "stars": 3}
+    
+    
+
+    
+    
+
