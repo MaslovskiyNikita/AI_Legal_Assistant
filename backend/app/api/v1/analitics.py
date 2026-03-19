ed764e2a8555b9b0291b031c266440e1e683df19
MaslovskiyNikita <nikitamaslovskiy999@gmail.com>
2026-03-19 21:07:23 +0400

    statistic update

--- backend/app/api/v1/analitics.py
+++ backend/app/api/v1/analitics.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from loguru import logger
 
 from backend.app.api.dependencies import get_db
+from backend.app.services.statistic_service import get_risk_statistic
 
 
 router = APIRouter(prefix="/api/v1/analytics", tags=["Analytics"])
@@ -11,7 +12,7 @@ router = APIRouter(prefix="/api/v1/analytics", tags=["Analytics"])
 async def get_risk_statistics(user_id: int, db: AsyncSession = Depends(get_db)):
     logger.info("📊 Запрос статистики рисков")
     
-    risk_stats = await get_risk_statistics(db, user_id)
+    risk_stats = await get_risk_statistic(db, user_id)
     
     logger.info(f"📈 Статистика рисков: {risk_stats}")
     return risk_stats
